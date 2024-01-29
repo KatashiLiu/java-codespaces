d7a2ff69f5cf7f13161e223e52ad72aa154bb338
KatashiLiu <156875331+KatashiLiu@users.noreply.github.com>
2024-01-29 07:48:53 +0000

    Leetcode 138 21

---
+++ src/main/java/com/mycompany/app/test138.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+public class test138 {
+    public Node copyRandomList(Node head) {
+        
+    }
+}
+
+
